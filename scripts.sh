1367e102616e8dbf9c4db31f663cdd8239e3d3f8
GeekEast <sx.ba2017@gmail.com>
2019-10-31 10:08:17 +1030

    fix naming

--- scripts.sh
+++ scripts.sh
@@ -1,21 +1,15 @@
-# MACPORT
-cmake -G "CodeBlocks - Unix Makefiles" \
-  -DBUILD_opencv_java=ON \ 
--DBUILD_SHARED_LIBS=OFF \ 
--DOPENCV_ENABLE_NONFREE=ON \ 
--DOPENCV_EXTRA_MODULES_PATH=/opt/local/var/macports/build/_opt_local_var_macports_sources_rsync.macports.org_macports_release_tarballs_ports_graphics_opencv/opencv/work/opencv_contrib-3.4.7/modules \ 
-
 # 1: 3.4.6
 cmake -D WITH_FFMPEG=ON -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_CXX_FLAGS="-Wno-c++11-narrowing" -D ENABLE_CXX11=ON -D BUILD_SHARED_LIBS:BOOL=OFF -D OPENCV_EXTRA_MODULES_PATH:PATH="/Users/xiangt/Desktop/opencv3.4.6/opencv_contrib-3.4.6/modules" -D OPENCV_ENABLE_NONFREE:BOOL=ON ..
-
+# relate library
 brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 x265 xvid nasm
 
+# check the system library path for C/C++
 gcc -v
-https://www.jianshu.com/p/5aa327282628
+# https://www.jianshu.com/p/5aa327282628
 
-4 parameters
+# Key Parameters
 WITH_FFMPEG
 BUILD_SHARED_LIBS
 OPENCV_EXTRA_MODULES_PATH
 OPENCV_ENABLE_NONFREE
-java
+JAVA
